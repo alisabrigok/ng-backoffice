cbfb396b88d1f027fc7e76d16a08636b976b11be
Ali Sabri GÖK <agok@stcpay.com.sa>
2020-06-23 19:21:50 +0300

    Create initial dashboard navbar

--- src/app/layouts/dashboard-layout/dashboard-layout.module.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 import { DashboardLayoutRoutingModule } from './dashboard-layout-routing.module';
 import { DashboardLayout } from './dashboard-layout.component';
 
 @NgModule({
-  imports: [CommonModule, DashboardLayoutRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardLayoutRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   declarations: [DashboardLayout],
 })
 export class DashboardLayoutModule {}

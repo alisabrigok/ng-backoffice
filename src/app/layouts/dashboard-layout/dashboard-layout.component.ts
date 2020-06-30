2c0453755d1d4beca1cacfc472a57dff1364a0ba
Ali Sabri GÖK <agok@stcpay.com.sa>
2020-06-30 18:41:20 +0300

    Make navigation bar mobile responsive

--- src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -9,16 +10,29 @@ import { Title } from '@angular/platform-browser';
 })
 export class DashboardLayout implements OnInit {
   title: String = this.route.snapshot.firstChild.data.title;
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    titleService: Title
+    titleService: Title,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
   ) {
     titleService.setTitle(`${this.title} - Backoffice`);
+
+    this.mobileQuery = media.matchMedia('(max-width: 1200px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   logout(): void {
     this.router.navigateByUrl('/login');
